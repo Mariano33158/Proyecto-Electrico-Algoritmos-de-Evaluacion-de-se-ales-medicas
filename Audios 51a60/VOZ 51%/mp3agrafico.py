f44b9f645ad222f551f7e63f66201e8b33826c1d
KevAgHt99 <Kagular9992@gmail.com>
2025-04-20 02:07:12 -0600

    Upload de los codigos de Python

--- Audios 51a60/VOZ 51%/mp3agrafico.py
+++ Audios 51a60/VOZ 51%/mp3agrafico.py
@@ -45,5 +45,5 @@ def plot_audio_waveform(filename):
 #)
 
 # Ejemplo de uso
-plot_audio_waveform('C:\\Users\\usuario\\Desktop\\UCR\\I-25\\Proyecto eléctrico\REPOSITORIO_CODES\\Proyecto-Electrico-Algoritmos-de-Evaluacion-de-se-ales-medicas\Audios 31a40\\VOZ 40%\\Voz40.mp3')
+plot_audio_waveform('C:\\Users\\usuario\\Desktop\\UCR\\I-25\\Proyecto eléctrico\REPOSITORIO_CODES\\Proyecto-Electrico-Algoritmos-de-Evaluacion-de-se-ales-medicas\Audios 51a60\\VOZ 51%\\Voz51.mp3')
 
